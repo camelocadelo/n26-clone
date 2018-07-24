795b5f507ed1daad98238ba35e1f860f0bcad140
hamzaouiii <hamzaouiii@hotmail.com>
2018-07-24 14:52:22 +0200

    created a login screen clone of n26

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+import Login from "@/components/Login.vue";
+
+export default {
+  name: "home",
+  components: {
+    Login
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  background: url('../assets/img/bg.jpg') fixed no-repeat;
+  background-size: 100% 100%;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  z-index: -1;
+  top: 0px;
+  right: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+</style>
