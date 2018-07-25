cef1183a384a29cd922fa73c42ddf6a9dc4ec0f7
hamzaouiii <hamzaouiii@hotmail.com>
2018-07-25 11:50:41 +0200

    added a nav

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Transactions from "./views/Transactions.vue";
+// import auth from "@/lib/login/";
 
 Vue.use(Router);
 
@@ -11,11 +13,17 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+      // beforeEnter: auth.requireAuth
     },
     {
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/transactions",
+      name: "transactions",
+      component: Transactions
     }
   ]
 });
