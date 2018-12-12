2c8f1afde7f2df64d13fdd8559e3e0f72538c01e
hamzaouiii <simo.hamzaoui@web.de>
2018-12-12 03:06:41 +0100

    Initial dist subtree commit

---
+++ precache-manifest.af02cb0efac150f4e306e97d17339b92.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b37510baba2994f6b1df9fcdd010ba69",
+    "url": "img/card.b37510ba.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "aecd3b2e2d89ab5857cdff8225c92db5",
+    "url": "img/de.aecd3b2e.svg"
+  },
+  {
+    "revision": "79c741c4a70696f80326",
+    "url": "chunk-vendors.79c741c4.js"
+  },
+  {
+    "revision": "878c783f1086f51cdacbe2873ced5169",
+    "url": "img/everest.878c783f.jpg"
+  },
+  {
+    "revision": "0b2c8ef0b7e6d183e4c4dd6b00060c5b",
+    "url": "img/fr.0b2c8ef0.svg"
+  },
+  {
+    "revision": "1660bbde3781cced6415cc82f4db232b",
+    "url": "img/main_account.1660bbde.jpg"
+  },
+  {
+    "revision": "16c3cb16c406f876fd1b4cf57baa36ec",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "b7a502d7ff31526f9bb0792a464feb78",
+    "url": "img/sidebar-bg.b7a502d7.jpg"
+  },
+  {
+    "revision": "7a616c7345096e1e0800ce447ef3f6ed",
+    "url": "img/en.7a616c73.svg"
+  },
+  {
+    "revision": "20f91d5825e546413bd8300758ee4f6b",
+    "url": "img/es.20f91d58.svg"
+  },
+  {
+    "revision": "711a1f6f1ec27b9e5711268f3d19d065",
+    "url": "img/it.711a1f6f.svg"
+  },
+  {
+    "revision": "f88f2e18e681ebfc2ba1e814fb7b14af",
+    "url": "index.html"
+  },
+  {
+    "revision": "6c89c7cbdfb399d0f10b",
+    "url": "app.8c151f9c.css"
+  },
+  {
+    "revision": "6c89c7cbdfb399d0f10b",
+    "url": "app.6c89c7cb.js"
+  }
+];
