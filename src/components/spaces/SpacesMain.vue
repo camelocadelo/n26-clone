00ea21f0fd25b7290f116606073a2fd803ded006
hamzaouiii <simo.hamzaoui@web.de>
2018-12-12 13:25:24 +0100

    drag and drop between spaces

--- src/components/spaces/SpacesMain.vue
+++ src/components/spaces/SpacesMain.vue
@@ -1,36 +1,50 @@
 <template>
   <ul>
     <li>
-      <div class="spaces__square">
-          <img src="@/assets/img/main_account.jpg">
+      
+      <div class="spaces__square"  draggable="true" @dragstart="drag($event)">
+        <div class="spaces__square__overlay">
         </div>
-        <div class="spaces__details">
-          <span class="spaces__details__name" >Main Account</span>
-          <span class="spaces__details__balance">€229.34</span>
+        <img src="@/assets/img/main_account.jpg">
+      </div>
+      <div class="spaces__details">
+        <span class="spaces__details__name">Main Account</span>
+        <span class="spaces__details__balance">€229.34</span>
+      </div>
+    </li>
+    <li @drop="drop($event)" @dragover.prevent>
+      <div class="spaces__square" >
+        <div class="spaces__square__overlay">
         </div>
-        
+        <img src="https://cdn.number26.de/spaces/default-images/education_books.jpg?version=1">
+      </div>
+      <div class="spaces__details">
+        <span class="spaces__details__name">Vacation</span>
+        <span class="spaces__details__balance">€24</span>
+      </div>
     </li>
     <li>
-        <div class="spaces__square">
-          <img src="https://cdn.number26.de/spaces/default-images/education_books.jpg?version=1">
-        </div>
-        <div class="spaces__details">
-          <span class="spaces__details__name" >Vacation</span>
-          <span class="spaces__details__balance">€24</span>
-        </div>  
-    </li>
-    <li>
-        <button class="spaces__create-new">
-          <i class="far fa-plus-square"></i>
-          <span>Create a new space</span>
-        </button>
-          
+      <button class="spaces__create-new">
+        <i class="far fa-plus-square"></i>
+        <span>Create a new space</span>
+      </button>
     </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: "SpacesMain"
+  name: "SpacesMain",
+  methods: {
+    drag() {
+      // eslint-disable-next-line
+      console.log("dragin");
+    },
+    drop() {
+      // eslint-disable-next-line
+
+      console.log("dropping");
+    }
+  }
 };
 </script>
