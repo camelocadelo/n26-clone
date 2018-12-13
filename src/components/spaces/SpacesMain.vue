dc25039351e67ee35bd668fca7122aa4822604a6
hamzaouiii <simo.hamzaoui@web.de>
2018-12-14 00:32:25 +0100

    routing to transfer view when dropped at a space

--- src/components/spaces/SpacesMain.vue
+++ src/components/spaces/SpacesMain.vue
@@ -48,6 +48,8 @@ export default {
     drop() {
       event.preventDefault();
       this.dragged = false;
+      this.$router.push({ name: 'transfer', query: { spaceName: 'Vacation' }  });
+
     }
   }
 };
