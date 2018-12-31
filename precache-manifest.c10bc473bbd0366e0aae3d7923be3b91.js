3ef7e0f86707a3ffa68b6e81d64860270f2f12c0
Simo Hamzaoui <simo.hamzaoui@web.de>
2018-12-31 02:48:43 +0100

    new build

--- precache-manifest.c10bc473bbd0366e0aae3d7923be3b91.js
+++ precache-manifest.c10bc473bbd0366e0aae3d7923be3b91.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = [
   {
-    "revision": "aecd3b2e2d89ab5857cdff8225c92db5",
-    "url": "img/de.aecd3b2e.svg"
+    "revision": "b37510baba2994f6b1df9fcdd010ba69",
+    "url": "img/card.b37510ba.jpg"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
     "url": "robots.txt"
   },
   {
-    "revision": "7a616c7345096e1e0800ce447ef3f6ed",
-    "url": "img/en.7a616c73.svg"
+    "revision": "aecd3b2e2d89ab5857cdff8225c92db5",
+    "url": "img/de.aecd3b2e.svg"
   },
   {
     "revision": "79c741c4a70696f80326",
     "url": "chunk-vendors.79c741c4.js"
   },
   {
-    "revision": "878c783f1086f51cdacbe2873ced5169",
-    "url": "img/everest.878c783f.jpg"
+    "revision": "4f3638429dc49c8f7ff936e10834813d",
+    "url": "img/rever.4f363842.jpg"
+  },
+  {
+    "revision": "d652a16b929c67ab057e3f6ce0f343db",
+    "url": "img/white.d652a16b.jpg"
   },
   {
     "revision": "0b2c8ef0b7e6d183e4c4dd6b00060c5b",
@@ -28,16 +32,16 @@ self.__precacheManifest = [
     "url": "img/main_account.1660bbde.jpg"
   },
   {
-    "revision": "b37510baba2994f6b1df9fcdd010ba69",
-    "url": "img/card.b37510ba.jpg"
-  },
-  {
     "revision": "16c3cb16c406f876fd1b4cf57baa36ec",
     "url": "favicon.png"
   },
   {
     "revision": "b7a502d7ff31526f9bb0792a464feb78",
     "url": "img/sidebar-bg.b7a502d7.jpg"
+  },
+  {
+    "revision": "7a616c7345096e1e0800ce447ef3f6ed",
+    "url": "img/en.7a616c73.svg"
   },
   {
     "revision": "7433a7d31cfc5d60bf21f44a45bc2ef1",
@@ -52,15 +56,15 @@ self.__precacheManifest = [
     "url": "img/it.711a1f6f.svg"
   },
   {
-    "revision": "d96b181673d4d33ccc7b60d6ad3f6210",
+    "revision": "6318fca7c7d37a6f019eac4c922d40ba",
     "url": "index.html"
   },
   {
-    "revision": "875c561263f70142f511",
-    "url": "app.a69f6923.css"
+    "revision": "4c68f3662d229fd7511a",
+    "url": "app.f897f67b.css"
   },
   {
-    "revision": "875c561263f70142f511",
-    "url": "app.875c5612.js"
+    "revision": "4c68f3662d229fd7511a",
+    "url": "app.4c68f366.js"
   }
 ];
